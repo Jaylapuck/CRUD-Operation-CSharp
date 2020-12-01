e0b2101774d260c9855601ec0f69a707c4a4f0c9
Jean-Christophe Benoît <jc_ben@live.ca>
2020-12-01 12:35:40 -0500

    Added Person class and DBhandler class with the readallpersons method

---
+++ FinalProject/DBHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class DBHandler
+    {
+        string ConnStr = ConfigurationManager.ConnectionStrings["ContactConn"].ConnectionString;
+
+        public List<Person> ReadAllPersons()
+        {
+            List<Person> personList = new List<Person>();
+
+            using (SqlConnection conn = new SqlConnection(ConnStr))
+            {
+                conn.Open();
+                SqlCommand cm = new SqlCommand("select * from Person", conn);
+
+                using (SqlDataReader reader = cm.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Person person = new Person();
+
+                        if (Int32.TryParse(reader["Id"].ToString(), out int id))
+                        {
+                            person.Id = id;
+                        }
+
+                        person.FirstName = reader["FirstName"].ToString();
+                        person.LastName = reader["LastName"].ToString();
+                        person.City = reader["City"].ToString();
+
+                        if (Int32.TryParse(reader["Age"].ToString(), out int age))
+                        {
+                            person.Age = age;
+                        }
+
+                        personList.Add(person);
+                    }
+                }
+            }
+
+            return personList;
+        }
+    }
+}
---
+++ FinalProject/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return "ID: " + this.Id + " | First Name: " + this.FirstName + " | Last Name: " + this.LastName + " | City: " + this.City + " | Age: " + this.Age + " |\n";
+        }
+    }
+}
+}
